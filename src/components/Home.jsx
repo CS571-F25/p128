05b3d7094466a5200143168c846f202fd6179c58
Jake Soko <sokolowskijake55@gmail.com>
2025-11-04 21:37:20 -0600

    initial publish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 export default function Home (props){
     return <div>
-        <h1>Home</h1>
+        <h1>Fishing site</h1>
+        <img style={{width:500}} src="/p128/fish.JPEG" alt="An image of fish"></img>
     </div>
 }
